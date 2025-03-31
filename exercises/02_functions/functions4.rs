c6a0fad222875122d99c6ad44710032b065b6ffe
aarontta-psk <207921825+aarontta-psk@users.noreply.github.com>
2025-03-31 13:24:18 +0200

    rustlings exercise: functions

--- exercises/02_functions/functions4.rs
+++ exercises/02_functions/functions4.rs
@@ -8,15 +8,15 @@ fn is_even(num: i64) -> bool {
 }
 
 // TODO: Fix the function signature.
-fn sale_price(price: i64) -> {
+fn sale_price(price: i64) -> i64 {
     if is_even(price) {
-        price - 10
+        return price - 10;
     } else {
-        price - 3
+        return price - 3;
     }
 }
 
 fn main() {
-    let original_price = 51;
+    let original_price: i64 = 51;
     println!("Your sale price is {}", sale_price(original_price));
 }
