46dfdd9305c522b41cdca1ce269b8efe6180bb52
aarontta-psk <207921825+aarontta-psk@users.noreply.github.com>
2025-04-07 02:02:15 +0200

    rustlings exercise: threads

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -17,10 +17,11 @@ impl Queue {
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     // TODO: We want to send `tx` to both threads. But currently, it is moved
     // into the first thread. How could you solve this problem?
+    let tx_clone = tx.clone();
     thread::spawn(move || {
         for val in q.first_half {
             println!("Sending {val:?}");
-            tx.send(val).unwrap();
+            tx_clone.send(val.clone()).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
@@ -28,7 +29,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     thread::spawn(move || {
         for val in q.second_half {
             println!("Sending {val:?}");
-            tx.send(val).unwrap();
+            tx.send(val.clone()).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
